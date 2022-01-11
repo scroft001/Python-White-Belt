11f04a5e7076a690dd6e625da02cc507a62551c0
Steven <scroft001@gmail.com>
2022-01-10 16:53:58 -0800

    Added project code

---
+++ main.py
@@ -0,0 +1,17 @@
+youtube = input("Are you subbed to PewDiePie or T-Series? ")
+
+if(youtube == "PewDiePie"):
+    print("Yay! You are making the world a better place.")
+elif(youtube == "pewdiepie"):
+    print("Yay! You are making the world a better place.")
+else:
+    print("Sub to PewDiePie")
+
+youtube2 = input("Are you subbed to Hackingtons? ")
+
+if(youtube2 == "Yes"):
+    print("Yay! You are making the world a better place.")
+elif(youtube2 == "yes"):
+    print("Yay! You are making the world a better place.")
+else:
+    print("Why you booly me")
